ec52a732650be76ab0e3ebd9d61973bc151754ce
PeterHo83 <68497446+PeterHo83@users.noreply.github.com>
2021-07-20 21:35:32 +1200

    Implement Food Menu using React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import MenuCard from './menu-card/menu-card';
+import Categories from './categories/categories';
+import items from './data/data';
 import './App.css';
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const allCategories = ['all', ...new Set(items.map((item) => item.category))];
+    setCategories(allCategories);
+  }, []);
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <MenuCard items={menuItems} />
+      </section>
+    </main>
   );
 }
 
